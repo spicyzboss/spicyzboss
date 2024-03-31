f43a587ee4589041b72ba0c427d589938ec9a6d4
spicyzboss <supachai@spicyz.io>
2024-03-31 11:44:52 +0700

    feat(root): add additional security header

--- apps/root/src/routes/plugin@security.ts
+++ apps/root/src/routes/plugin@security.ts
@@ -1,9 +1,22 @@
 import { RequestHandler } from "@builder.io/qwik-city";
 
 export const onRequest: RequestHandler = ({ headers }) => {
+  const permissions = [
+    'accelerometer=()',
+    'camera=()',
+    'geolocation=()',
+    'gyroscope=()',
+    'magnetometer=()',
+    'microphone=()',
+    'payment=()',
+    'usb=()'
+  ];
+
   const securityHeaders = {
     'X-Frame-Options': 'sameorigin',
     'X-XSS-Protection': '1; mode=block',
+    'Referrer-Policy': 'same-origin',
+    'Permissions-Policy': permissions.join('; '),
   };
 
   Object.entries(securityHeaders).map(([key, value]) => headers.set(key, value));
